af76a301fb8e023860f2d9b0cdd17937b0d782d8
m-sulkouski@gmail.com <maxim.sulkouski@gmail.com>
2020-01-09 20:38:43 +0300

    Initial changes to source code

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,66 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] resumeStorage = new Resume[10000];
+    private int resumeCounter = 0;
 
     void clear() {
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        resumeStorage[this.resumeCounter] = resume;
+        this.resumeCounter++;
+        cleanResumeStorage();
     }
 
     Resume get(String uuid) {
+        int index = findResume(uuid);
+        if (index != -1)
+            return resumeStorage[index];
         return null;
     }
 
     void delete(String uuid) {
+        int index = findResume(uuid);
+        if (index != -1)
+
+            ;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return this.resumeStorage;
     }
 
     int size() {
         return 0;
+    }
+
+    private int findResume (String uuid) {
+        int counter = 0;
+        while (counter < this.resumeCounter) {
+            if (resumeStorage[counter].getUuid().equals(uuid)) {
+                return counter;
+            }
+        }
+        return -1;
+    }
+
+    private void cleanResumeStorage() {
+        Resume[] processedResumeStorage = new Resume[this.resumeCounter];
+        int processedIndex = 0;
+        for (int i = 0; i < resumeCounter; i++, processedIndex++) {
+            if (resumeStorage[i] == null) {
+                if (i < resumeCounter - 1)
+                    i++;
+                else {
+                    break;
+                }
+            }
+            processedResumeStorage[processedIndex] = resumeStorage[i];
+        }
+        this.resumeStorage = processedResumeStorage;
     }
 }
